c0a73023500acfe947fd1a216b6242c64482cd26
bjsavino <bruno.judiss@gmail.com>
2020-10-16 10:26:34 -0300

    Adicionar arquivos de projeto.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Aruco
+{
+   partial class MainForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.components = new System.ComponentModel.Container();
+            this.printArucoBoardButton = new System.Windows.Forms.Button();
+            this.cameraButton = new System.Windows.Forms.Button();
+            this.useThisFrameButton = new System.Windows.Forms.Button();
+            this.cameraImageBox = new Emgu.CV.UI.ImageBox();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxW = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxU = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxV = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printArucoBoardButton
+            // 
+            this.printArucoBoardButton.Location = new System.Drawing.Point(9, 8);
+            this.printArucoBoardButton.Margin = new System.Windows.Forms.Padding(2);
+            this.printArucoBoardButton.Name = "printArucoBoardButton";
+            this.printArucoBoardButton.Size = new System.Drawing.Size(110, 27);
+            this.printArucoBoardButton.TabIndex = 0;
+            this.printArucoBoardButton.Text = "Print Aruco Board";
+            this.printArucoBoardButton.UseVisualStyleBackColor = true;
+            this.printArucoBoardButton.Click += new System.EventHandler(this.printArucoBoardButton_Click);
+            // 
+            // cameraButton
+            // 
+            this.cameraButton.Location = new System.Drawing.Point(155, 8);
+            this.cameraButton.Margin = new System.Windows.Forms.Padding(2);
+            this.cameraButton.Name = "cameraButton";
+            this.cameraButton.Size = new System.Drawing.Size(204, 27);
+            this.cameraButton.TabIndex = 1;
+            this.cameraButton.Text = "Open Camera";
+            this.cameraButton.UseVisualStyleBackColor = true;
+            this.cameraButton.Click += new System.EventHandler(this.cameraButton_Click);
+            // 
+            // useThisFrameButton
+            // 
+            this.useThisFrameButton.Enabled = false;
+            this.useThisFrameButton.Location = new System.Drawing.Point(381, 8);
+            this.useThisFrameButton.Margin = new System.Windows.Forms.Padding(2);
+            this.useThisFrameButton.Name = "useThisFrameButton";
+            this.useThisFrameButton.Size = new System.Drawing.Size(176, 27);
+            this.useThisFrameButton.TabIndex = 3;
+            this.useThisFrameButton.Text = "Use this frame";
+            this.useThisFrameButton.UseVisualStyleBackColor = true;
+            this.useThisFrameButton.Click += new System.EventHandler(this.useThisFrameButton_Click);
+            // 
+            // cameraImageBox
+            // 
+            this.cameraImageBox.Location = new System.Drawing.Point(9, 73);
+            this.cameraImageBox.Margin = new System.Windows.Forms.Padding(2);
+            this.cameraImageBox.Name = "cameraImageBox";
+            this.cameraImageBox.Size = new System.Drawing.Size(868, 500);
+            this.cameraImageBox.TabIndex = 2;
+            this.cameraImageBox.TabStop = false;
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(9, 46);
+            this.messageLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(35, 13);
+            this.messageLabel.TabIndex = 4;
+            this.messageLabel.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(936, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Enabled = false;
+            this.textBoxX.Location = new System.Drawing.Point(956, 81);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 6;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Enabled = false;
+            this.textBoxY.Location = new System.Drawing.Point(956, 107);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(936, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Enabled = false;
+            this.textBoxZ.Location = new System.Drawing.Point(956, 133);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(100, 20);
+            this.textBoxZ.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(936, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Z";
+            // 
+            // textBoxW
+            // 
+            this.textBoxW.Enabled = false;
+            this.textBoxW.Location = new System.Drawing.Point(956, 237);
+            this.textBoxW.Name = "textBoxW";
+            this.textBoxW.Size = new System.Drawing.Size(100, 20);
+            this.textBoxW.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(936, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "W";
+            // 
+            // textBoxU
+            // 
+            this.textBoxU.Enabled = false;
+            this.textBoxU.Location = new System.Drawing.Point(956, 185);
+            this.textBoxU.Name = "textBoxU";
+            this.textBoxU.Size = new System.Drawing.Size(100, 20);
+            this.textBoxU.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(936, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "U";
+            // 
+            // textBoxV
+            // 
+            this.textBoxV.Enabled = false;
+            this.textBoxV.Location = new System.Drawing.Point(956, 211);
+            this.textBoxV.Name = "textBoxV";
+            this.textBoxV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxV.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(936, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "V";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 487);
+            this.Controls.Add(this.textBoxV);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxU);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxW);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.useThisFrameButton);
+            this.Controls.Add(this.cameraImageBox);
+            this.Controls.Add(this.cameraButton);
+            this.Controls.Add(this.printArucoBoardButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "Aruco demo";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button printArucoBoardButton;
+      private System.Windows.Forms.Button cameraButton;
+      private Emgu.CV.UI.ImageBox cameraImageBox;
+      private System.Windows.Forms.Button useThisFrameButton;
+      private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxW;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxU;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxV;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
